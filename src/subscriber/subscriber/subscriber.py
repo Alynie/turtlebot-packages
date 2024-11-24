265d40ca84b29f3d585bda7a99b3f788bed5a85a
StKateHAB <161260185+StKateHAB@users.noreply.github.com>
2024-11-24 11:49:36 -0600

    use examples

--- src/subscriber/subscriber/subscriber.py
+++ src/subscriber/subscriber/subscriber.py
@@ -1,29 +1,37 @@
 import rclpy
 from rclpy.node import Node
+
 from std_msgs.msg import String
 
+
 class MinimalSubscriber(Node):
+
     def __init__(self):
         super().__init__('minimal_subscriber')
-        print("init")
-        self.subscription = self.create_subscription(String, 'topic', self.listener_callback, 10)
-        self.subscription #prevent unused variable warning
-        print("init end")
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        print("callback")
         self.get_logger().info('I heard: "%s"' % msg.data)
-        print('I heard: "%s"' % msg.data)
+
 
 def main(args=None):
     rclpy.init(args=args)
 
     minimal_subscriber = MinimalSubscriber()
+
     rclpy.spin(minimal_subscriber)
 
-    #Destroy the node explicitly so that garbage collector doesn't need to do it
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
