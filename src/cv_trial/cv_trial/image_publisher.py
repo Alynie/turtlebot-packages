a47820d99b2016a9042ff2664ac0f35f7cc49db6
StKateHAB <161260185+StKateHAB@users.noreply.github.com>
2024-12-06 18:48:50 -0600

    60 sec

--- src/cv_trial/cv_trial/image_publisher.py
+++ src/cv_trial/cv_trial/image_publisher.py
@@ -21,7 +21,7 @@ class ImagePublisher(Node):
         if not self.cam.isOpened():
             raise Exception("Camera open failed!")
         self.br = CvBridge()
-        self.timer = self.create_timer(30, self.publish_images)
+        self.timer = self.create_timer(60, self.publish_images)
         print("init end")
 
     def publish_images(self):
