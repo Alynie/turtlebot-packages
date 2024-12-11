6f95ad59f281c2a60949ce1d0dca76ff335ab999
StKateHAB <161260185+StKateHAB@users.noreply.github.com>
2024-12-11 15:48:27 -0600

    update num classes

--- src/final/final/detect_gesture.py
+++ src/final/final/detect_gesture.py
@@ -11,13 +11,29 @@ class Gesture():
         cfg = get_cfg()
         cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
         cfg.DATASETS.TRAIN = ("mdata1_train",)
-        self.metadata_train = MetadataCatalog.get("mdata1_train").set(thing_classes=["fist", "palm", "ok", "tnf", "one_finger_left", "one_finger_right"])
+        self.metadata_train = MetadataCatalog.get("mdata1_train").set(thing_classes=["fist", "palm", "ok", "tnf", "one_finger_left", "one_finger_right", "no_gesture"])
         cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  
 
-        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 7
         cfg.MODEL.DEVICE='cpu'
         cfg.MODEL.WEIGHTS = "train/95percentBW.pth"  # path to the model we just trained
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+        
+        # cfg = get_cfg()
+        # cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        # cfg.DATASETS.TRAIN = ("mdata1_train")
+        # cfg.DATASETS.TEST = ()
+        # cfg.DATALOADER.NUM_WORKERS = 2
+        # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+        # cfg.SOLVER.IMS_PER_BATCH = 2
+        # cfg.SOLVER.BASE_LR = 0.001  # pick a good LR
+        # cfg.SOLVER.MAX_ITER = 1750    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+        # cfg.SOLVER.STEPS = []        # do not decay learning rate
+        # cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64   # 256 is faster, and good enough for this toy dataset (default: 512)
+        # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 7
+        # cfg.OUTPUT_DIR = weights_dir
+        # cfg.SEED = 42
+        
         self.predictor = DefaultPredictor(cfg)
         print("=================Finish Init========================")
 
